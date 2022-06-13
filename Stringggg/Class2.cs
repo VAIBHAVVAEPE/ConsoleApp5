ea5d0f0b597101f44b9feb1bb21764b680aad08e
VAIBHAVVAEPE <vaibhavvarpe1997@gmail.com>
2022-06-13 13:45:01 +0530

    Add project files.

---
+++ Stringggg/Class2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stringggg
+{
+    internal class Class222
+    {
+        static void Main(string[] args)
+        {
+            int c = 1;
+            for (int i = 1; i <5; i++)
+
+            {
+                
+
+                for (int j = 1; j<=i; j++)
+                {
+
+                    Console.Write(c+" ");
+                    c++;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
